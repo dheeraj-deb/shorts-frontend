52cd2d308353eca8c393280ec1121e8ca3fda3dc
dheeraj-deb <dheerajt9539@gmail.com>
2022-12-01 17:53:52 +0530

    change base url

--- src/util/Axios.js
+++ src/util/Axios.js
@@ -4,7 +4,7 @@ import axios from "axios";
 const token = JSON.parse(localStorage.getItem("user"))?.accessToken
 
 const Axios = axios.create({
-  baseURL: "http://localhost:4000/shorts/api",
+  baseURL: "http://18.183.45.105/shorts/api",
   headers: {
     Authorization: token ? `Bearer ${token}` : null,
   },
